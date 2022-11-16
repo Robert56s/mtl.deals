3dec0c93f7f1b9b310d5c151970669669144b46c
Robert56s <78372422+Robert56s@users.noreply.github.com>
2022-11-16 03:54:14 +0000

    navbar progress

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,46 @@
-<nav>
-  <a href="/">Home</a>
-  <a href="/about">About</a>
-  <a href="/settings">Settings</a>
-</nav>
+<script>
+
+</script>
+
+<div class="navbar">
+  <a href="/"><img src="/logo.png" alt="logo"></a>
+  <div class="button">
+    <a href="/" data-sveltekit-prefetch>Home</a>
+    <a href="/about" data-sveltekit-prefetch>About</a>
+    <a href="/" data-sveltekit-prefetch>Setting</a>
+  </div>
+</div>
 
 <slot></slot>
+
+<style>
+  img{
+    max-height: 150px;
+  }
+  
+  /* The navigation bar */
+.navbar {
+  overflow: hidden;
+  background-color: #333;
+  position: fixed; /* Set the navbar to fixed position */
+  top: 0; /* Position the navbar at the top of the page */
+  width: 100%; /* Full width */
+}
+
+/* Links inside the navbar */
+.navbar a {
+  float: left;
+  display: block;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+/* Change background on mouse-over */
+.button a:hover {
+  background: #ddd;
+  color: black;
+}
+
+</style>
