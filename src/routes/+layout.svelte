144cd8e7510014cb28cd1d4535cfd4e6451bd4d2
Robert <robertnelea56@gmail.com>
2022-11-28 22:54:10 -0500

    navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,46 +1,29 @@
-<script>
-
-</script>
-
 <div class="navbar">
-  <a href="/"><img src="/logo.png" alt="logo"></a>
-  <div class="button">
-    <a href="/" data-sveltekit-prefetch>Home</a>
-    <a href="/about" data-sveltekit-prefetch>About</a>
-    <a href="/" data-sveltekit-prefetch>Setting</a>
-  </div>
+  <a href="/"><h1>mtl.deals</h1></a>
+  <nav class="buttons">
+    <ul>
+      <li>
+        <a href="/" data-sveltekit-prefetch>Home</a>
+      </li>
+      <li>
+        <a href="/about" data-sveltekit-prefetch>About</a>
+      </li>
+      <li>
+        <a href="/" data-sveltekit-prefetch>Setting</a>
+      </li>
+    </ul>
+  </nav>
 </div>
 
 <slot></slot>
 
 <style>
-  img{
-    max-height: 150px;
+
+  .navbar {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background: grey;
   }
-  
-  /* The navigation bar */
-.navbar {
-  overflow: hidden;
-  background-color: #333;
-  position: fixed; /* Set the navbar to fixed position */
-  top: 0; /* Position the navbar at the top of the page */
-  width: 100%; /* Full width */
-}
-
-/* Links inside the navbar */
-.navbar a {
-  float: left;
-  display: block;
-  color: #f2f2f2;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-}
-
-/* Change background on mouse-over */
-.button a:hover {
-  background: #ddd;
-  color: black;
-}
 
 </style>
