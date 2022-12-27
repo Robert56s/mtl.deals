15197df6220396cc572d3c411c5f8880360bdafd
Robert <robertnelea56@gmail.com>
2022-12-26 23:35:07 -0500

    buckets

--- src/routes/settings/+page.server.js
+++ src/routes/settings/+page.server.js
@@ -1,22 +1,42 @@
 import { redirect } from "@sveltejs/kit";
+// import { randomBytes } from "node:crypto";
 
 /** @type {import('./$types').PageServerLoad} */
-export async function load( { locals } ) {
+export async function load({ locals} ) {
 
     if (!locals.session) {
         throw redirect(303, '/')
     }
 
-    let { data: profiles, error } = await locals.sb
-    .from('profiles')
-    .select('username')
-    .eq('id', locals.session.user.id)
-    
-    profiles = profiles[0]
+    const getUsername = async () => {
+        let { data: username, error } = await locals.sb
+        .from('profiles')
+        .select('username')
+        .eq('id', locals.session.user.id)
+
+        username = username[0].username
+        return username
+
+    }
+
+    const getAvatar = async () => {
+        let { data: avatar_url, error: err1} = await locals.sb.from('profiles').select('avatar_url').eq('id', locals.session.user.id)
+        avatar_url = avatar_url[0].avatar_url
+        // randomBytes(24, function(err, buffer) {
+        //     var token = buffer.toString('hex');
+        //     console.log(token)
+        // });
+
+        let { data: avatar, error: err2 } = await locals.sb.storage.from('profiles').getPublicUrl(`${avatar_url}`)
+        avatar = avatar.publicUrl
+        return avatar
+    }
 
     return {
-        profiles: profiles
+        username: getUsername(),
+        avatar: getAvatar()
     }
+
 }
 
 /** @type {import('./$types').Actions} */
@@ -26,8 +46,6 @@ export const actions = {
 
         //convert form entries data to an object
         const body = Object.fromEntries(await request.formData())
-
-        console.log(body)
         
         const { data, error: err } = await locals.sb
         .from('profiles')
--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -2,11 +2,12 @@
     export let data;
 </script>
 <div>
+    <img src="{data.avatar}" alt="avatar">
     <h1>Edit username</h1>
     <form action="?/accupdate" method="post">
         <h4>username</h4>
         <label for="username">
-            <input name="username" type="username" value="{data.profiles.username}">
+            <input name="username" type="username" value="{data.username}">
         </label>
         <button>save</button>
         
@@ -24,5 +25,11 @@
     button {
         margin: 0.5rem;
         padding: 0.5rem 1rem;
+    }
+
+    img {
+        width: 50px;
+        height: 50px;
+        border-radius: 50%;
     }
 </style>
